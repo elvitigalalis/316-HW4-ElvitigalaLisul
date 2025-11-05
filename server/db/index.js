d537e6be2f4ec19778695f3655bd6c56b14eeb2e
elvitigalalis <elvitigalalis@gmail.com>
2025-11-04 20:47:07 -0500

    Fix package issues

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,14 @@
-require(dotenv).config({ path: __dirname + '/../../.env' });
+require("dotenv").config({ path: __dirname + "/../../.env" });
 let DatabaseManager;
 
 if (process.env.DB_TYPE === "mongodb") {
-    const MongoDatabaseManager = require("./mongodb");
-    DatabaseManager = new MongoDatabaseManager();
+  const MongoDatabaseManager = require("./mongodb");
+  DatabaseManager = new MongoDatabaseManager();
 } else if (process.env.DB_TYPE === "postgresql") {
-    const PostgreDatabaseManager = require ("./postgresql");
-    DatabaseManager = new PostgreDatabaseManager();
+  const PostgreDatabaseManager = require("./postgresql");
+  DatabaseManager = new PostgreDatabaseManager();
 } else {
-    throw new Error("Unsupported DB_TYPE in .env file: " + process.env.DB_TYPE);
+  throw new Error("Unsupported DB_TYPE in .env file: " + process.env.DB_TYPE);
 }
 
 DatabaseManager.connect();
